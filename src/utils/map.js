4330cac74132781a98f72d7fb07a7588bf06d489
xLong <381612175@qq.com>
2021-12-25 18:35:15 +0800

    优化

--- src/utils/map.js
+++ src/utils/map.js
@@ -1,4 +1,3 @@
-import proj4 from "proj4";
 import wkids from "mock/wkids.json";
 
 /**
@@ -8,41 +7,11 @@ import wkids from "mock/wkids.json";
  * @param {*} widget 部件
  */
 export function removeWidget(view, widget) {
-  if (widget) {
-    if (view) {
-      view.ui.remove(widget);
+    if (widget) {
+        if (view) {
+            view.ui.remove(widget);
+        }
+        widget.destroy();
+        widget = null;
     }
-    widget.destroy();
-    widget = null;
-  }
-}
-
-/**
- * 坐标转换
- *
- * @param {*} sourceWkid 源坐标系
- * @param {*} toWkid 目标坐标系
- * @param {*} x x坐标
- * @param {*} y y坐标
- *
- */
-export function convertCoord(sourceWkid, toWkid, x, y) {
-  try {
-    if (!wkids[sourceWkid] && !wkids[toWkid]) {
-      return null;
-    }
-
-    // proj4.defs(
-    //   `EPSG:${toWkid}`,
-    //   wkids[toWkid]
-    // );
-
-    return proj4(wkids[sourceWkid], wkids[toWkid], [
-      parseFloat(x),
-      parseFloat(y),
-    ]);
-  } catch (e) {
-    console.log(e);
-    return null;
-  }
 }
