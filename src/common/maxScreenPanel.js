bf0e8104216a3bd8833659c6503b9e324bc418ba
xLong <381612175@qq.com>
2021-12-26 09:28:00 +0800

    新增：release配置

--- src/common/maxScreenPanel.js
+++ src/common/maxScreenPanel.js
@@ -7,74 +7,75 @@
 import { onMounted } from "@vue/runtime-core";
 import { ref } from "@vue/reactivity";
 
-const VITE_APP_ENV = import.meta.env.VITE_APP_ENV;
-const VITE_APP_BASE_URL = import.meta.env.VITE_APP_BASE_URL;
+const VITE_APP_ENV =
+    import.meta.env.VITE_APP_ENV;
+const VITE_APP_BASE_URL =
+    import.meta.env.VITE_APP_BASE_URL;
 
+export default function() {
+    const url = ref("");
 
-export default function () {
-  const url = ref("");
-
-  onMounted(() => {
-    setUrl();
-  });
-
-  // 配置地址
-  const setUrl = () => {
-    // 开发环境使用本地地址
-    if (VITE_APP_ENV === "development") {
-      url.value = window.location.origin;
-    } else {
-      url.value = VITE_APP_BASE_URL;
-    }
-  };
-
-  /**
-   * 关闭面板
-   * 
-   * @param {*} emit 触发事件
-   * @param {*} props 组件接受参数
-   */
-  const onClosePanel = (emit, props) => {
-    emit("close", {
-      panel: props.panel,
-      index: props.index,
-      active: false,
+    onMounted(() => {
+        setUrl();
     });
-  };
 
-  /**
-   * 面板最小
-   * 
-   * @param {*} emit 触发事件
-   * @param {*} props 组件接受参数
-   */
-  const onMinimizePanel = (emit, props) => {
-    emit("minimize", {
-      panel: props.panel,
-      index: props.index,
-      active: true,
-    });
-  };
+    // 配置地址
+    const setUrl = () => {
+        // 开发环境使用本地地址
+        if (VITE_APP_ENV === "development") {
+            url.value = window.location.origin;
+        } else {
+            url.value = VITE_APP_BASE_URL;
+        }
+    };
 
-  /**
-   * 面板最大化
-   * 
-   * @param {*} emit 触发事件
-   * @param {*} props 组件接受参数
-   */
-  const onMaximizePanel = (emit, props) => {
-    emit("maximize", {
-      panel: props.panel,
-      index: props.index,
-      active: false,
-    });
-  };
+    /**
+     * 关闭面板
+     * 
+     * @param {*} emit 触发事件
+     * @param {*} props 组件接受参数
+     */
+    const onClosePanel = (emit, props) => {
+        emit("close", {
+            panel: props.panel,
+            index: props.index,
+            active: false,
+        });
+    };
 
-  return {
-    url,
-    setUrl,
-    onClosePanel,
-    onMinimizePanel,
-    onMaximizePanel
-  };
+    /**
+     * 面板最小
+     * 
+     * @param {*} emit 触发事件
+     * @param {*} props 组件接受参数
+     */
+    const onMinimizePanel = (emit, props) => {
+        emit("minimize", {
+            panel: props.panel,
+            index: props.index,
+            active: true,
+        });
+    };
+
+    /**
+     * 面板最大化
+     * 
+     * @param {*} emit 触发事件
+     * @param {*} props 组件接受参数
+     */
+    const onMaximizePanel = (emit, props) => {
+        emit("maximize", {
+            panel: props.panel,
+            index: props.index,
+            active: false,
+        });
+    };
+
+    return {
+        url,
+        setUrl,
+        onClosePanel,
+        onMinimizePanel,
+        onMaximizePanel
+    };
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import LayoutDefault from "@/layout/default/index.vue";
 import { createRouter, createWebHistory } from "vue-router";
 
+const VITE_APP_ENV =
+    import.meta.env.VITE_APP_ENV;
+
 /**
  * 静态路由
  */
@@ -44,7 +47,7 @@ Object.keys(routesModules).forEach((modulePath) => {
  */
 export const initRouter = () =>
     createRouter({
-        history: createWebHistory('/xlong-map-admin/'),
+        history: createWebHistory(VITE_APP_ENV === 'test' ? '' : '/xlong-map-admin/'),
         scrollBehavior: () => ({ y: 0 }),
         routes: [...constantRoutes],
     });
